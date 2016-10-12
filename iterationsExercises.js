dd10110c622db9a895332db4927fa0030bae5283
App Academy Student <student@appacademy.io>
2016-10-12 14:29:11 -0700

    add iteration exercises

---
+++ iterationsExercises.js
@@ -0,0 +1,34 @@
+function bubbleSort(array){
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true
+    for (let i = 0; i < array.length; i++) {
+      let j = i + 1
+      if (array[i] > array[j]) {
+        sorted = false
+        let bigger = array[i]
+        let smaller = array[j]
+        array[i] = smaller
+        array[j] = bigger
+      }
+    }
+  }
+  return array
+}
+
+// console.log(bubbleSort([7,4,9,6,2,1]));
+
+
+function substrings(string) {
+  let subs = [];
+  for (let i = 0; i < string.length; i++) {
+    for (var j = 0; j <= string.length; j++) {
+      if (i < j) {
+        subs.push(string.slice(i,j))
+      }
+    }
+  }
+  return subs;
+}
+console.log(substrings("cat"));
